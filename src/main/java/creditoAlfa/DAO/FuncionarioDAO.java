93c69eedf98749e77fc3d30b885067917e2715de
Matheus Biasi <biasimatheushenrique40@gmail.com>
2022-11-01 15:21:48 -0300

    Refatorando metodo que verifica se CPF ja existe

--- src/main/java/creditoAlfa/DAO/FuncionarioDAO.java
+++ src/main/java/creditoAlfa/DAO/FuncionarioDAO.java
@@ -1,11 +1,21 @@
 package creditoAlfa.DAO;
 
+import javax.persistence.TypedQuery;
+
 import creditoAlfa.model.Funcionario;
 
 public class FuncionarioDAO extends GenericDAO<Funcionario, Long> {
 
 	public FuncionarioDAO() {
 		super(Funcionario.class);
+	}
+
+	public Funcionario buscaByCPF(String cpfFuncionario) {
+		String query =  " SELECT funcionario FROM Funcionario funcionario "
+				+ " WHERE cpf = :cpfFuncionario ";
+		TypedQuery<Funcionario> typedQuery = entityManager.createQuery(query,Funcionario.class);
+		typedQuery.setParameter("cpfFuncionario", cpfFuncionario);
+		return typedQuery.getSingleResult();
 	}
 
 }
